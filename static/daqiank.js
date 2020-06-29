dadbd7b6529eff44fe035f2e729d563352498486
216teachman <66990120+216teachman@users.noreply.github.com>
2020-06-29 21:29:11 +0800

    Update daqiank.js

--- static/daqiank.js
+++ static/daqiank.js
@@ -1,25 +1,51 @@
 var _hmt = _hmt || []; 
-if ($(".bdsharebuttonbox").length) {
-	if ($(".article-content").length) {
-		$(".article-content img").data("tag", "bdshare")
-	}
-	window._bd_share_config = {
-		common: {
-			"bdText": "",
-			"bdMini": "2",
-			"bdMiniList": false,
-			"bdPic": "",
-			"bdStyle": "0",
-			"bdSize": "24"
-		},
-		share: [{
-			bdCustomStyle: "/style/css/share.css"
-		}]
-	};
-	with(document) {
-		0[(getElementsByTagName("head")[0] || body).appendChild(createElement("script")).src = "http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion=" + ~ ( - new Date() / 3600000)]
-	}
-}
+$(document).ready(function() {
+     c = {
+        url: document.URL,
+        pic: $('meta[property="og:image"]').attr("content"),
+        title: document.title || "",
+        desc: $('meta[name="description"]').length ? $('meta[name="description"]').attr("content") : ""
+    }
+    $(".share-weixin").each(function() {
+        $(this).find(".share-popover").length || ($(this).append('<span class="share-popover"><span class="share-popover-inner" id="weixin-qrcode"></span></span>'),
+        $("#weixin-qrcode").qrcode({
+            width: 80,
+            height: 80,
+            text: c.url
+        }))
+    })
+    $('[etap="share"]').on("click", function() {
+        var a = $(this)
+          , b = a.data("share")
+          , d = "";
+        switch (b) {
+        case "qq":
+            d = "http://connect.qq.com/widget/shareqq/index.html?url=" + c.url + "&desc=" + c.desc + "&summary=" + c.title + "&site=zeshlife&pics=" + c.pic;
+            break;
+        case "weibo":
+            d = "http://service.weibo.com/share/share.php?title=" + c.title + "&url=" + c.url + "&source=bookmark&pic=" + c.pic;
+            break;
+        case "douban":
+            d = "http://www.douban.com/share/service?image=" + c.pic + "&href=" + c.url + "&name=" + c.title + "&text=" + c.desc;
+            break;
+        case "qzone":
+            d = "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=" + c.url + "&title=" + c.title + "&desc=" + c.desc + "&pics=" + c.pic;
+            break;
+        case "tqq":
+            d = "http://share.v.t.qq.com/index.php?c=share&a=index&url=" + c.url + "&title=" + c.title;
+            break;
+        case "facebook":
+            d = "https://www.facebook.com/share.php?u=" + c.url + "&t=" + c.title;
+            break;
+        case "twitter":
+            d = "https://twitter.com/intent/tweet?text=" + c.title + c.url;
+            break;
+        case "linkedin":
+            d = "https://www.linkedin.com/shareArticle?mini=true&ro=true&armin=armin&url=" + c.url + "&title=" + c.title + "&summary=" + c.desc
+        }
+        a.attr("href") || a.attr("target") || a.attr("href", d).attr("target", "_blank")
+    })
+})
 if ($(".thumb:first").data("src") || $(".widget_ui_posts .thumb:first").data("src")) {
 	$(".thumb").lazyload({
 		data_attribute: "src",
